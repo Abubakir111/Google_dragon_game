81f07bb482c3ac9990e6925c51b29a93b9b5368f
Бакир Базаркул <bakirbazarkul16@gmail.com>
2023-12-13 02:51:56 +0300

    Google dragon game version 1

---
+++ game.js
@@ -0,0 +1,22 @@
+const dino = document.querySelector('#dragon')
+const cacktus = document.querySelector('#cacktus')
+
+const jump = () => {
+  if (dino.classList != 'jump') {
+    dino.classList.add('jump')
+  }
+  setTimeout(() => {
+    dino.classList.remove('jump')
+  }, 300)
+}
+document.addEventListener('keydown', () => jump())
+count = 0
+let isActive = setInterval(() => {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
+  let calcktusLeft = parseInt(
+    window.getComputedStyle(cacktus).getPropertyValue('left')
+  )
+  if (calcktusLeft < 80 && calcktusLeft > 60 && dinoTop >= 369) {
+    alert('Game over')
+  }
+}, 10)
